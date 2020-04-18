6dc1dd3fda2faefe816aee479ac96b7391446e70
Luke Curtis <l.curtis@baypath.net>
2020-04-17 22:52:57 -0400

    Changed Data to RGBvalues that looks good on white or black, Began Testing(with 2,000,000 iterations)

--- test.py
+++ test.py
@@ -2,27 +2,40 @@ from Neuron import *
 from NeuralNetMath import *
 from NeuralNetLayer import *
 from NeuralNetwork import *
-n1 = Neuron("Length")
-n2 = Neuron("Width")
+n1 = Neuron("Red")
+n2 = Neuron("Green")
+n3 = Neuron("Blue")
 
-neurons = [n1,n2]
+neurons = [n1,n2,n3]
 inputLayer = NeuralNetLayer(neurons)
-nn = NeuralNetwork([inputLayer],5000,0.1)
+nn = NeuralNetwork([inputLayer],2000000,0.0001)
 
-mystery = [4.5, 1]
+mystery = [0,255,255]
 
-data = [[3,   1.5, 1],
-        [2,   1,   0],
-        [4,   1.5, 1],
-        [3,   1,   0],
-        [3.5, .5,  1],
-        [2,   .5,  0],
-        [5.5,  1,  1],
-        [1,    1,  0]]
+data = [
+		#Colors That Look Good in Black
+		[255,255,0,1],#yellow
+		[0,0,255,1],#blue
+		[255,0,0,1],#red
+		[0,255,0,1],#green
+		[128,0,128,1],#purple
+		[218,165,32,1],#golden
+		[255,255,255,1],#white
+		[0,255,255,1],#cyan
+		[255,0,255,1],#magenta
+		[192,192,192,1],#silver
+		#Colors That Look good on White
+		[128,0,0,0],#maroon
+		[0,128,0,0],#green
+		[0,0,0,0],#black
+		[128,128,128,0],#gray
+		[0,128,128,0],#teal
+		[0,0,128,0]#Navy
+		]
 
 nn.train_all(data)
 
-if(sigmoid(nn.neuralNetLayers[0].getPrediction([4.5,1])) < 0.5):
-	print("Blue")
+if(sigmoid(nn.neuralNetLayers[0].getPrediction(mystery)) < 0.5):
+	print("White")
 else:
-	print("Red")
+	print("Black")
