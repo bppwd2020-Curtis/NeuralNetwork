484cca8be070a75a6c0b1fede7a129aeca92458b
Luke Curtis <l.curtis@baypath.net>
2020-05-04 22:07:01 -0400

    Final Commit of the day

--- visualization.py
+++ visualization.py
@@ -7,7 +7,7 @@ size = width, height = 320, 240
 speed = [2, 2]
 white = 255, 255, 255
 
-screen = pygame.display.set_mode(pygame.display.list_modes(depth=0, flags=pygame.FULLSCREEN)[1])
+screen = pygame.display.set_mode(pygame.display.list_modes(depth=0, flags=pygame.FULLSCREEN)[0])
 
 while 1:
     for event in pygame.event.get():
