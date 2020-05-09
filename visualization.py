9a16699ed1f0364db044025296dd5b986b8d76d8
Luke Curtis <l.curtis@baypath.net>
2020-05-09 18:04:37 -0400

    Added Text and Implemented NN into visualization training at 90000000 iterations

--- visualization.py
+++ visualization.py
@@ -1,7 +1,37 @@
 import os,sys
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
+import numpy as np
+from Neuron import *
+from NeuralNetMath import *
+from NeuralNetLayer import *
+from NeuralNetwork import *
 
+#Neural Net Part
+n1 = Neuron("Red")
+n2 = Neuron("Green")
+n3 = Neuron("Blue")
+
+neurons = [n1,n2,n3]
+inputLayer = NeuralNetLayer(neurons)
+nn = NeuralNetwork([inputLayer],900000,0.001)
+
+mystery = [2.55,2.55,2.55]
+
+data = [
+		#Colors That Look Good in Black
+		[2.55,2.55,0,1],#yellow
+		[0,2.55,0,1],#lime
+		[2.55,2.55,2.55,1],#white
+		[0,2.55,2.55,1],#cyan
+		#Colors That Look good on White
+		[1.28,0,0,0],#maroon
+		[0,0,0,0],#black
+		[1.28,1.28,1.28,0],#gray
+		[0,0,1.28,0]#Navy
+		]
+
+nn.train_all(data, reLu)
 
 
 
@@ -18,6 +48,12 @@ blue = (0,0,255)
 clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode(pygame.display.list_modes(depth=0, flags=pygame.FULLSCREEN)[2])
+pygame.display.set_caption("RGB Neural Network")
+
+def draw_text(string,size,color,coord):
+	font = pygame.font.SysFont("arial",size)
+	text = font.render(string,True,color)
+	screen.blit(text,(coord[0] - text.get_width() // 2, coord[1] - text.get_height() // 2))
 
 def draw_red(r):
 	#draws based on red value
@@ -39,14 +75,16 @@ def draw_rgb(r,g,b):
 	draw_green(g) 
 	draw_blue(b) 
 
-def draw_RGB(rgb):
-	#Use when passed as an array
-	draw_red(rgb[0])
-	draw_green(rgb[1])
-	draw_blue(rgb[2])
+def draw_output(r,g,b,back):
+	rect = pygame.Rect(screen.get_width()/2,0,screen.get_width()/2,screen.get_height())
+	pygame.draw.rect(screen,back,rect,0)
+	draw_text("Test",50,(r,g,b),rect.center)
 
-def draw_output(r,g,b):
-	pygame.draw.rect(screen,(r,g,b),(screen.get_width()/2,0,screen.get_width()/2,screen.get_height()),0)
+def draw_total(r,g,b,back):
+	draw_rgb(r,g,b)
+	draw_output(r,g,b,back)
+
+
 
 while True:
     for event in pygame.event.get():
@@ -54,8 +92,15 @@ while True:
 
     clock.tick(30) 
     screen.fill(white)
-    draw_output(128,0,128)
-    
+
+    mystery = [255,255,255]
+
+    if(reLu(nn.neuralNetLayers[0].getPrediction(mystery)) <= 0):
+    	back = white
+    elif(reLu(nn.neuralNetLayers[0].getPrediction(mystery)) > 0):
+   		back = black
+
+    draw_total(mystery[0],mystery[1],mystery[2],back)
 
 
 
