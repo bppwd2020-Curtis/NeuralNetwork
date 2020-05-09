281256fdce900f9df6be7a6aced8ebf326e93f69
Luke Curtis <l.curtis@baypath.net>
2020-05-09 16:58:55 -0400

    Added Green and BLue Methods

--- visualization.py
+++ visualization.py
@@ -20,7 +20,18 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode(pygame.display.list_modes(depth=0, flags=pygame.FULLSCREEN)[2])
 
 def draw_red(r):
-	pygame.draw.rect(screen,red,(0,0,screen.get_width()/2,screen.get_height()/3),0)
+	#draws based on red value
+	pygame.draw.rect(screen,(r,0,0),(0,0,screen.get_width()/2,screen.get_height()/3),0)
+
+def draw_green(g):
+	#draws based on green value
+	green_y = (screen.get_height()/3)
+	pygame.draw.rect(screen,(0,g,0),(0,green_y,screen.get_width()/2,screen.get_height()/3),0)
+
+def draw_blue(b):
+	#draws based on blue value
+	blue_y = 2 * (screen.get_height()/3)
+	pygame.draw.rect(screen,(0,0,b),(0,blue_y,screen.get_width()/2,screen.get_height()/3),0)
 
 while True:
     for event in pygame.event.get():
@@ -29,5 +40,12 @@ while True:
     clock.tick(30) 
     screen.fill(white)
     draw_red(255)
+    draw_green(255)
+    draw_blue(255)
+    
+
+
+
+
     pygame.display.flip()
 
