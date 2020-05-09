484cd6b8392f76fcfdcafa114c783de7fd684c51
Luke Curtis <l.curtis@baypath.net>
2020-05-09 16:47:37 -0400

    Added Red rectangle

--- visualization.py
+++ visualization.py
@@ -1,17 +1,33 @@
 import os,sys
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
+
+
+
+
 pygame.init()
 
 size = width, height = 320, 240
 speed = [2, 2]
-white = 255, 255, 255
+white = (255, 255, 255)
+black = (0,0,0)
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
 
-screen = pygame.display.set_mode(pygame.display.list_modes(depth=0, flags=pygame.FULLSCREEN)[0])
+clock = pygame.time.Clock()
 
-while 1:
+screen = pygame.display.set_mode(pygame.display.list_modes(depth=0, flags=pygame.FULLSCREEN)[2])
+
+def draw_red(r):
+	pygame.draw.rect(screen,red,(0,0,screen.get_width()/2,screen.get_height()/3),0)
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
+    clock.tick(30) 
     screen.fill(white)
+    draw_red(255)
     pygame.display.flip()
+
